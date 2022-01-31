a9d7808ff67612956652e6356e70bb7425b40eec
Marc Teixeira <MTeixeira@my.gcu.edu>
2022-01-30 20:42:58 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalStoreApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
