2ef9941ec39b957bf6b43caefc5c563b4b8234f7
Marc Teixeira <MTeixeira@my.gcu.edu>
2022-02-08 08:22:21 -0800

    Hooked up About page to Home controller.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PersonalStoreApplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
