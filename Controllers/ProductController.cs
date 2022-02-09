6ff2b7df8bddb5027b6047f8b51449c881769065
Marc Teixeira <MTeixeira@my.gcu.edu>
2022-02-08 18:17:30 -0800

    Added Product controller and initial search page.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalStoreApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessSearch(string token)
+        {
+            return View("Index", "Home");
+        }
+
+        public PartialViewResult GetPartialProductCard(Product product)
+        {
+            return PartialView("_productCard", product);
+        }
+    }
+}
