4f19f44ba69475113f0d75d31aa7b0159f0e08f4
MTeixeiraGCU <MTeixeira@my.gcu.edu>
2022-02-17 19:01:42 -0800

    First pass of checkout module.

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalStoreApplication.BusinessServices;
+
+namespace PersonalStoreApplication.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        ProductBusinessService pbs;
+
+        public CheckoutController(ProductBusinessService pbs)
+        {
+            this.pbs = pbs;
+        }
+
+        [CustomAuthorization(LogoutRequired = false)]
+        public IActionResult Index()
+        {
+            
+            return View("Checkout", pbs.GetUsersCart((int)HttpContext.Session.GetInt32("userId")));
+        }
+
+        [CustomAuthorization(LogoutRequired = false)]
+        public IActionResult Addresses()
+        {
+            return View("Addresses", null);
+        }
+    }
+}
