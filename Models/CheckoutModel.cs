eab34697a2d26ea426d877023ada7d68a4287d62
MTeixeiraGCU <MTeixeira@my.gcu.edu>
2022-02-18 12:36:36 -0800

    Added process checkout button.

--- Models/CheckoutModel.cs
+++ Models/CheckoutModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace PersonalStoreApplication.Models
 {
@@ -6,11 +7,14 @@ namespace PersonalStoreApplication.Models
     {
         public List<CartItemDTO> CartList { get; set; }
         public List<Address> Addresses { get; set; }
+
+        [DisplayName("Payment Type:")]
         public PaymentType Payment { get; set; }
     }
 
     public enum PaymentType
     {
-        Credit = 0
+        Credit = 0,
+        Debit = 1
     }
 }
