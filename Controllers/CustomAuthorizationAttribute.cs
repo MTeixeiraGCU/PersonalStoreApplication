379146b26dcc5dd0166da500342c845cdb3881a3
MTeixeiraGCU <MTeixeira@my.gcu.edu>
2022-03-14 13:34:01 -0700

    Added lockout status to session variable.

--- Controllers/CustomAuthorizationAttribute.cs
+++ Controllers/CustomAuthorizationAttribute.cs
@@ -13,6 +13,8 @@ namespace PersonalStoreApplication.Controllers
         public bool LogoutRequired { get; set; }
         public bool AdminRequired { get; set; }
 
+        public const int MAX_ATTEMPTS = 10;
+
         /// <summary>
         /// This method checks for a valid user id in the sessions variables. If one does not exist, it redirects to the login page.
         /// </summary>
@@ -21,7 +23,14 @@ namespace PersonalStoreApplication.Controllers
         {
             int? userId = context.HttpContext.Session.GetInt32("userId");
             int? role = context.HttpContext.Session.GetInt32("userRole");
+            int? attemptStatus = context.HttpContext.Session.GetInt32("attemptStatus");
             bool isAdmin = false;
+
+            //check for lockout status
+            if(attemptStatus != null && (int)attemptStatus >= MAX_ATTEMPTS)
+            {
+                context.Result = new RedirectResult("/Login/LockedOut");
+            }
 
             //check administrator status
             if(role != null)
